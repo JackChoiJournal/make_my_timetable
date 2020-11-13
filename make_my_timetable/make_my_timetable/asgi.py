24af948d7ad86d49e69a18c7db58cf88a4b29618
Jack Choi <jackchoiprogrammer@gmail.com>
2020-11-14 01:29:51 +1030

    feat: Initialize the project
    - Installed Django 3.1.3
    - Installed python-decouple
    - Separate development and production settings
    - Add template path
    - Modified base.py

---
+++ make_my_timetable/make_my_timetable/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for make_my_timetable project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'make_my_timetable.settings')
+
+application = get_asgi_application()
